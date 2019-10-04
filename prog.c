f46ca54477bd42697e0cfa36eb331c7a0e0a4ea4
Lê Tuấn Đạt <48952542+tuandat2110@users.noreply.github.com>
2019-10-04 17:05:42 +0700

    Add files via upload

--- prog.c
+++ prog.c
@@ -26,23 +26,20 @@ char **split(char *s,char *delim)
 	char **ptrs;
 	unsigned int psize,nword=1,slen=strlen(s),delen=strlen(delim);
 	while((_s=strstr(_s,delim))){
-		_s+=delen;
-		++nword;
+	_s+=delen;
+	++nword;
 	}
 	psize=(nword+1)*sizeof(char*);
 	ptrs=data=malloc(psize+slen+1);
-	if(data)
-	{
-		*ptrs=_s=strcpy(((char*) data) +psize,s);
-		if(nword>1)
-		{
-			while((_s=strstr(_s,delim))){
-			*_s='\0';
-			_s+=delen;
-			*++ptrs=_s;
-			}
-		}
-		*++ptrs=NULL;
+	if(data){
+	*ptrs=_s=strcpy(((char*) data) +psize,s);
+	if(nword>1){
+	while((_s=strstr(_s,delim))){
+	*_s='\0';
+	_s+=delen;
+	*++ptrs=_s;
+	}}
+	*++ptrs=NULL;
 	}
 	return data;
 }
@@ -50,119 +47,152 @@ int main(void)
 {
 	char **args;
 	char temp[100];
+	char temp1[100];
 	int run=1;
-	
+	int cp=1;
+	int his=0;
 	while(run)
 	{
 	
+		
 		printf("osh>");
 		fflush(stdout);
 		
 		gets(temp);
 		
-		/*for(int i=0;i<=strlen(temp);i++)
-		{
-			
-			if(temp[i]==' '||i==strlen(temp))
-			{
-				
-				args[k]=substr(temp,t,i-1);
-				
-				t=i+1;
-				k++;
-			}
-		}*/
+		
 		args=split(temp," ");
 		if(args==NULL)
 			continue;
+		if(strstr(temp,"!!"))
+			cp=0;
+		else
+			cp=1;
+		int u=0;
+		int t=1;
+		while(args[u+1]!=NULL)
+		{
+				
+			u++;
+		}
+		if(strcmp(args[u],"&")==0)
+		
+		{
+			t=0;
+			printf("not wait");
+			args[u]=NULL;
+		}
 		if(strcmp(args[0],"exit")==0)
 		{
 			run=0;
 			
 		}
-		else
+else
+{
+		pid_t id=fork();
+		if(id==0)
 		{
-			pid_t id=fork();
-			if(id==0)
+			int i=0;
+			int k=0;
+			if (strcmp(args[0],"!!")==0)
 			{
-				int i=0;
-				int k=0;
-				while(args[i]!=NULL)
+				if(his==0)
 				{
+					printf("No command in history\n");
+					exit(1);
+				}
+				else
+				{
+					
+					free(args);
+					args=split(temp1," ");
+				}
+			}
+			while(args[i]!=NULL)
+			{
 				
-					if(strcmp(args[i],">")==0)
+				if(strcmp(args[i],">")==0)
+				{
+					int file_desc=open(args[i+1],O_WRONLY|O_CREAT);
+					dup2(file_desc,1);
+					while(args[i]!=NULL)
 					{
-						int file_desc=open(args[i+1],O_WRONLY|O_CREAT);
-						dup2(file_desc,1);
+						args[i]=NULL;
+						i++;
+					}
+					break;
+				}
+				else
+				if(strcmp(args[i],"<")==0)
+				{
+					
+					strcpy(args[i],args[i+1]);
+					args[i+1]=NULL;
+					break;
+				}
+				else
+				if(strcmp(args[i],"|")==0)
+				{
+					int p1[2];
+					int p2[2];
+					pipe(p1);
+					pipe(p2);
+					int r=fork();
+					if(r==0)
+					{
+						execvp(args[i+1],args);
+						dup2(p1[1],1);
+						close(p1[1]);
+						close(p1[0]);
+						dup2(p2[0],0);
+				
+					}
+					else
+					{
 						while(args[i]!=NULL)
 						{
 							args[i]=NULL;
 							i++;
 						}
-						break;
+						execvp(args[0],args);
+						dup2(p2[1],1);
+						close(p2[0]);
+						close(p2[1]);
+						dup2(p1[0],0);
 					}
-					else
-					if(strcmp(args[i],"<")==0)
-					{
-					
-						strcpy(args[i],args[i+1]);
-						args[i+1]=NULL;
-						break;
-					}
-					else
-					if(strcmp(args[i],"|")==0)
-					{
-						int p1[2];
-						int p2[2];
-						pipe(p1);
-						pipe(p2);
-						int r=fork();
-						if(r==0)
-						{
-							execvp(args[i+1],args);
-							dup2(p1[1],1);
-							close(p1[1]);
-							close(p1[0]);
-							dup2(p2[0],0);
-				
-						}
-						else
-						{
-							while(args[i]!=NULL)
-							{
-								args[i]=NULL;
-								i++;
-							}
-							execvp(args[0],args);
-							dup2(p2[1],1);
-							close(p2[0]);
-							close(p2[1]);
-							dup2(p1[0],0);
-						}
-						exit(1);
-					}
-					i++;
+					exit(1);
 				}
+				i++;
+			}
 			
 			
 			
-				execvp(args[0],args);
-				exit(1);
+			execvp(args[0],args);
+			
+			exit(1);
 
+		}
+		else
+		{
+			if(t==1)
+			{
+				
+				wait(NULL);
+				
 			}
 			else
 			{
-				int i=0;
-				while(args[i+1]!=NULL)
-				{
-				
-					i++;
-				}
-				if(strcmp(args[i],"&")!=0)
-					wait(NULL);
+				exit(1);	
 			}
+				
 		}
-		free (args);
+	}
+	if(cp==1)
+	{
+		strcpy(temp1,temp);
+		his=1;
+	}
+	free (args);
 		
 	}
 	
